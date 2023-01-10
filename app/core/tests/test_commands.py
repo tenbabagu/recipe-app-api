80dcdc3cf65b86654048415f75b39f385eba9807
tenbabagu@gmail.com <tenbabagu@gmail.com>
2023-01-10 00:39:42 -0800

    update db test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,25 +10,25 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('django.db.connection.ensure_connection')
 class CommandTest(SimpleTestCase):
     """Test commands."""
 
-    def test_wait_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_ensure_connection):
         """Test waiting for database if database ready"""
-        patched_check.return_value = True
+        patched_ensure_connection.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_ensure_connection.assert_called_once()
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_ensure_connection):
         """Test waiting for database when getting OperationalError"""
-        patched_check.side_effect = [
+        patched_ensure_connection.side_effect = [
             Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        self.assertEqual(patched_ensure_connection.call_count, 6)
+        patched_ensure_connection.assert_called()
