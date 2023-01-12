5a5537a70aced78418c96d6823224fb8fd629da0
tenbabagu@gmail.com <tenbabagu@gmail.com>
2023-01-11 22:56:08 -0800

    fix format

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,16 +4,18 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTests(TestCase):
-  """Test mdodels"""
-  def test_create_user_with_email_successful(self):
-    """Test creating a user with an email is successful"""
-    email = "test@example.com"
-    password = "testpass123"
-    user = get_user_model().objects.create_user(
-      email = email,
-      password = password
-    )
 
-    self.assertEqual(user.email, email)
-    self.assertTrue(user.check_password(password))
+class ModelTests(TestCase):
+    """Test mdodels"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful"""
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
