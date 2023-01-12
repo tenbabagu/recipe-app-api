89b306caaa4d58c4c5957a5c0edd9551c2b7c867
tenbabagu@gmail.com <tenbabagu@gmail.com>
2023-01-11 23:01:53 -0800

    lint

--- app/core/models.py
+++ app/core/models.py
@@ -3,30 +3,31 @@ Database models
 """
 from django.db import models
 from django.contrib.auth.models import (
-	AbstractBaseUser,
-	BaseUserManager,
-	PermissionsMixin
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
 )
 
-class UserManager(BaseUserManager):
-	"""Manager for users. """
-	def create_user(self, email, password=None, **extra_field):
-		"""Create, save and return a new user."""
-		user = self.model(email=email, **extra_field)
-		user.set_password(password)
-		user.save(using=self._db)
 
-		return user
+class UserManager(BaseUserManager):
+    """Manager for users. """
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""User in the system. """
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default = True)
-	is_staff = models.BooleanField(default=False)
+    """User in the system. """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
+    objects = UserManager()
 
-	USERNAME_FIELD = 'email'
-
+    USERNAME_FIELD = 'email'
